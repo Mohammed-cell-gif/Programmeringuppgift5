f2ec9c6cba320fdd40ddca4aabb249fc63e2f648
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2025-09-24 08:26:12 +0200

    Programmeringuppgift5

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Skriv fem tall åt gång och jag kommer skriva dom i omvänd ordning");
+int[] v = new int[5];
+v[0] = int.Parse(Console.ReadLine());
+Console.WriteLine("Skriv de andra tal");
+v[1] = int.Parse(Console.ReadLine());
+Console.WriteLine("Skriv den tredje");
+v[2] = int.Parse(Console.ReadLine());
+Console.WriteLine("Skriv den fjärde");
+v[3] = int.Parse(Console.ReadLine());
+Console.WriteLine("Skriv den femte");
+v[4] = int.Parse(Console.ReadLine());
+Console.WriteLine(v[4]);
+Console.WriteLine(v[3]);
+Console.WriteLine(v[2]);
+Console.WriteLine(v[1]);
+Console.WriteLine(v[0]);
